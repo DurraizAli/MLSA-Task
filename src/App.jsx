7d8216817eaa3e78f2d3a7487aafe24a6514c014
MAON <durraizbukhari1234@gmail.com>
2023-11-12 12:17:46 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Durraiz Ali",
+  title: "Android Developer & UI/UX designer",
+  email: "Durraiz.Ali@studentambassadors.com",
+  gitHub: "DurraizAli",
+  instagram: "syed_durraiz_ali_/",
+  linkedIn: "durraiz-ali-6390b6219/",
+  medium: "@durraizbukhari1234",
+  twitter: "DurraizShah",
+  youTube: "Microsoft",
 };
 
 const primaryColor = "#4E567E";
